21b751f657700cb5bbaccf7099d6f03edcf32a45
amandachen007 <chenliangchi001@gmail.com>
2022-08-02 00:19:00 +1000

    TOPCODER

---
+++ Project1/src/data/content.js
@@ -0,0 +1,27 @@
+const CONTENT_INFO = {
+  island: {
+    h1: "Never Stop",
+    h2: "Explore The World",
+    p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae error recusandae ex placeat, dolor explicabo ab cupiditate odio delectus. Totam aliquid provident consequuntur mollitia. Voluptates dolore ut quidem aspernatur velit?"
+  },
+  spring: {
+    h1: "Spring",
+    h2: "Feel the new vitality",
+    p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae error recusandae ex placeat, dolor explicabo ab cupiditate odio delectus. Totam aliquid provident consequuntur mollitia. Voluptates dolore ut quidem aspernatur velit?"
+  },
+  summer: {
+    h1: "Summer",
+    h2: "Enjoy passionate time",
+    p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae error recusandae ex placeat, dolor explicabo ab cupiditate odio delectus. Totam aliquid provident consequuntur mollitia. Voluptates dolore ut quidem aspernatur velit?"
+  },
+  autumn: {
+    h1: "Autumn",
+    h2: "Experience without limits",
+    p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae error recusandae ex placeat, dolor explicabo ab cupiditate odio delectus. Totam aliquid provident consequuntur mollitia. Voluptates dolore ut quidem aspernatur velit?"
+  },
+  winter: {
+    h1: "Winter",
+    h2: "Enjoy peace and freedom",
+    p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae error recusandae ex placeat, dolor explicabo ab cupiditate odio delectus. Totam aliquid provident consequuntur mollitia. Voluptates dolore ut quidem aspernatur velit?"
+  },
+}
